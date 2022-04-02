6c5dfef0c8bd1d10a95b739f7e91ed405fe4cdaf
Nick Goncharenko <goncharenkonikolay@gmail.com>
2022-04-01 21:55:48 -0700

    feat: update

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -1,29 +1,31 @@
 import { MongoClient } from 'mongodb';
 
-export async function connectDatabase() {
-  const client = await MongoClient.connect(
-    'mongodb+srv://maximilian:8ZO3ycZqJ23kWBQx@cluster0.ntrwp.mongodb.net/events?retryWrites=true&w=majority'
-  );
+async function connectDatabase() {
+    try {
+        const client = await MongoClient.connect(
+            'mongodb+srv://nick:iuRzeKTFvsphU233@cluster.gybv3.mongodb.net/events?retryWrites=true&w=majority'
+        );
 
-  return client;
+        return client;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-export async function insertDocument(client, collection, document) {
-  const db = client.db();
+async function insertDocument(client, collection, document) {
+    const db = client.db();
 
-  const result = await db.collection(collection).insertOne(document);
+    const result = await db.collection(collection).insertOne(document);
 
-  return result;
+    return result;
 }
 
-export async function getAllDocuments(client, collection, sort) {
-  const db = client.db();
+async function getAllDocuments(client, collection, sort) {
+    const db = client.db();
 
-  const documents = await db
-    .collection(collection)
-    .find()
-    .sort(sort)
-    .toArray();
+    const documents = await db.collection(collection).find().sort(sort).toArray();
 
-  return documents;
+    return documents;
 }
+
+export { connectDatabase, getAllDocuments, insertDocument };
