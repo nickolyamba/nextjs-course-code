562dc77850e405cb531c6029facf0470bb0b016e
Nick Goncharenko <goncharenkonikolay@gmail.com>
2022-04-02 16:50:02 -0700

    feat: update packages

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -10,115 +10,107 @@ import Button from '../../components/ui/button';
 import ErrorAlert from '../../components/ui/error-alert';
 
 function FilteredEventsPage(props) {
-  const [loadedEvents, setLoadedEvents] = useState();
-  const router = useRouter();
+    const [loadedEvents, setLoadedEvents] = useState();
+    const router = useRouter();
 
-  const filterData = router.query.slug;
+    const filterData = router.query.slug;
 
-  const { data, error } = useSWR(
-    'https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json'
-  );
+    const { data, error } = useSWR('https://zz-prj-react-default-rtdb.firebaseio.com/events.json');
 
-  useEffect(() => {
-    if (data) {
-      const events = [];
+    useEffect(() => {
+        if (data) {
+            const events = [];
 
-      for (const key in data) {
-        events.push({
-          id: key,
-          ...data[key],
-        });
-      }
+            for (const key in data) {
+                events.push({
+                    id: key,
+                    ...data[key],
+                });
+            }
 
-      setLoadedEvents(events);
+            setLoadedEvents(events);
+        }
+    }, [data]);
+
+    let pageHeadData = (
+        <Head>
+            <title>Filtered Events</title>
+            <meta name='description' content={`A list of filtered events.`} />
+        </Head>
+    );
+
+    if (!loadedEvents) {
+        return (
+            <Fragment>
+                {pageHeadData}
+                <p className='center'>Loading...</p>
+            </Fragment>
+        );
     }
-  }, [data]);
 
-  let pageHeadData = (
-    <Head>
-      <title>Filtered Events</title>
-      <meta name='description' content={`A list of filtered events.`} />
-    </Head>
-  );
+    const filteredYear = filterData[0];
+    const filteredMonth = filterData[1];
 
-  if (!loadedEvents) {
-    return (
-      <Fragment>
-        {pageHeadData}
-        <p className='center'>Loading...</p>
-      </Fragment>
+    const numYear = +filteredYear;
+    const numMonth = +filteredMonth;
+
+    pageHeadData = (
+        <Head>
+            <title>Filtered Events</title>
+            <meta name='description' content={`All events for ${numMonth}/${numYear}.`} />
+        </Head>
     );
-  }
 
-  const filteredYear = filterData[0];
-  const filteredMonth = filterData[1];
+    if (
+        isNaN(numYear) ||
+        isNaN(numMonth) ||
+        numYear > 2030 ||
+        numYear < 2021 ||
+        numMonth < 1 ||
+        numMonth > 12 ||
+        error
+    ) {
+        return (
+            <Fragment>
+                {pageHeadData}
+                <ErrorAlert>
+                    <p>Invalid filter. Please adjust your values!</p>
+                </ErrorAlert>
+                <div className='center'>
+                    <Button link='/events'>Show All Events</Button>
+                </div>
+            </Fragment>
+        );
+    }
 
-  const numYear = +filteredYear;
-  const numMonth = +filteredMonth;
+    const filteredEvents = loadedEvents.filter((event) => {
+        const eventDate = new Date(event.date);
+        return eventDate.getFullYear() === numYear && eventDate.getMonth() === numMonth - 1;
+    });
 
-  pageHeadData = (
-    <Head>
-      <title>Filtered Events</title>
-      <meta
-        name='description'
-        content={`All events for ${numMonth}/${numYear}.`}
-      />
-    </Head>
-  );
+    if (!filteredEvents || filteredEvents.length === 0) {
+        return (
+            <Fragment>
+                {pageHeadData}
+                <ErrorAlert>
+                    <p>No events found for the chosen filter!</p>
+                </ErrorAlert>
+                <div className='center'>
+                    <Button link='/events'>Show All Events</Button>
+                </div>
+            </Fragment>
+        );
+    }
 
-  if (
-    isNaN(numYear) ||
-    isNaN(numMonth) ||
-    numYear > 2030 ||
-    numYear < 2021 ||
-    numMonth < 1 ||
-    numMonth > 12 ||
-    error
-  ) {
+    const date = new Date(numYear, numMonth - 1);
+
     return (
-      <Fragment>
-        {pageHeadData}
-        <ErrorAlert>
-          <p>Invalid filter. Please adjust your values!</p>
-        </ErrorAlert>
-        <div className='center'>
-          <Button link='/events'>Show All Events</Button>
-        </div>
-      </Fragment>
+        <Fragment>
+            {pageHeadData}
+            <ResultsTitle date={date} />
+            <EventList items={filteredEvents} />
+        </Fragment>
     );
-  }
-
-  const filteredEvents = loadedEvents.filter((event) => {
-    const eventDate = new Date(event.date);
-    return (
-      eventDate.getFullYear() === numYear &&
-      eventDate.getMonth() === numMonth - 1
-    );
-  });
-
-  if (!filteredEvents || filteredEvents.length === 0) {
-    return (
-      <Fragment>
-        {pageHeadData}
-        <ErrorAlert>
-          <p>No events found for the chosen filter!</p>
-        </ErrorAlert>
-        <div className='center'>
-          <Button link='/events'>Show All Events</Button>
-        </div>
-      </Fragment>
-    );
-  }
-
-  const date = new Date(numYear, numMonth - 1);
-
-  return (
-    <Fragment>
-      {pageHeadData}
-      <ResultsTitle date={date} />
-      <EventList items={filteredEvents} />
-    </Fragment>
-  );
 }
 
 // export async function getServerSideProps(context) {
